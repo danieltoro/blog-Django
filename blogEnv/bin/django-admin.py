ead248dfdfbeafd6ad59f83640eeec0f225986e6
Daniel Toro <d.toro@outlook.com>
2016-03-07 20:03:59 -0300

    Aplicacion con Django y Python

--- blogEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/danieltsx/Proyectos/Python/Django/ProyectoBlog/blogEnv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
